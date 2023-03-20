7d3661f6a9a78248321fcdb866acabc16c9f4416
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-20 13:48:01 -0300

    feat(organizationRepository): implement find by id method

--- server/src/application/repositories/organization-repository.ts
+++ server/src/application/repositories/organization-repository.ts
@@ -3,4 +3,5 @@ import { Organization } from '../entities/organization'
 export interface OrganizationRepository {
   create(organization: Organization): Promise<Organization>
   findByEmail(email: string): Promise<Organization | null>
+  findById(id: string): Promise<Organization | null>
 }
