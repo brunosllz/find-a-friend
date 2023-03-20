900962f955ca2b1eda772bebfc8f3da28993757e
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-20 08:23:50 -0300

    tests(organization): added validation to password hashed

--- server/src/application/use-cases/organization.spec.ts
+++ server/src/application/use-cases/organization.spec.ts
@@ -1,5 +1,7 @@
+import { compare } from 'bcryptjs'
 import { InMemoryOrganizationRepository } from 'test/repositories/in-memory-organization-repository'
 import { describe, expect, it, beforeEach } from 'vitest'
+import { OrganizationAlreadyExistsError } from './errors/organization-already-exists-error'
 import { OrganizationUseCase } from './organization'
 
 describe('Organization use case', () => {
@@ -24,7 +26,7 @@ describe('Organization use case', () => {
     expect(organization.id).toEqual(expect.any(String))
   })
 
-  it('Should be not able register a organization with a same email', async () => {
+  it('Should be not able register a organization with a same email twice', async () => {
     await sut.execute({
       name: 'organization example',
       email: 'organization@email.com',
@@ -43,6 +45,24 @@ describe('Organization use case', () => {
         cep: '99999000',
         phoneNumber: '99999999999',
       }),
-    ).rejects.toBeInstanceOf(Error)
+    ).rejects.toBeInstanceOf(OrganizationAlreadyExistsError)
+  })
+
+  it('Should hash password in the registration', async () => {
+    const { organization } = await sut.execute({
+      name: 'organization example',
+      email: 'organization@email.com',
+      password: '123456',
+      address: 'street example',
+      cep: '99999000',
+      phoneNumber: '99999999999',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      organization.password,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
   })
 })
