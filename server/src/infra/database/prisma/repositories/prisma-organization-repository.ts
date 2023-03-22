0156a9f133224a8957cb107f576a9a936b194d55
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-21 21:01:22 -0300

    feat(prisma): implement finds methods on repository

--- server/src/infra/database/prisma/repositories/prisma-organization-repository.ts
+++ server/src/infra/database/prisma/repositories/prisma-organization-repository.ts
@@ -1,7 +1,7 @@
 import { Organization } from '@/application/entities/organization'
 import { OrganizationRepository } from '@/application/repositories/organization-repository'
 import { PrismaClient } from '@prisma/client'
-import { PrismaOrganizationMapper } from './mappers/prisma-organization-mapper'
+import { PrismaOrganizationMapper } from '../mappers/prisma-organization-mapper'
 
 export class PrismaOrganizationRepository implements OrganizationRepository {
   constructor(private readonly prisma: PrismaClient) {}
@@ -16,11 +16,31 @@ export class PrismaOrganizationRepository implements OrganizationRepository {
     return PrismaOrganizationMapper.toDomain(rawOrganization)
   }
 
-  findByEmail(email: string): Promise<Organization | null> {
-    throw new Error('Method not implemented.')
+  async findByEmail(email: string) {
+    const organization = await this.prisma.organization.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!organization) {
+      return null
+    }
+
+    return PrismaOrganizationMapper.toDomain(organization)
   }
 
-  findById(id: string): Promise<Organization | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Organization | null> {
+    const organization = await this.prisma.organization.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!organization) {
+      return null
+    }
+
+    return PrismaOrganizationMapper.toDomain(organization)
   }
 }
