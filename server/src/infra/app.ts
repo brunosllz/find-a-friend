d735038ab8496704ea1f61360a1f0f4d2cf53b94
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-23 23:36:05 -0300

    feat(app): implement error handling and organization routes

--- server/src/infra/app.ts
+++ server/src/infra/app.ts
@@ -1,3 +1,24 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './config/env'
+import { organizationsRoutes } from './http/routes/organizations-routes'
 
 export const app = fastify()
+
+app.register(organizationsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Implement a external tool for track logs like a DataDog/NewRelic/Sentry.
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
--- server/src/infra/server.ts
+++ server/src/infra/server.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
+import { env } from './config/env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('🚀 Server is running!')
+    console.log(`🚀 Server is running!`)
   })
