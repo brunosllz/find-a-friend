be204cd102c1f9f510b9c4a8e23d2d832a39ea97
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-24 09:51:07 -0300

    tests(organization): added password validation

--- server/src/application/entities/organization.spec.ts
+++ server/src/application/entities/organization.spec.ts
@@ -1,22 +1,23 @@
+import { MakeOrganization } from 'test/factories/organization-factory'
 import { describe, expect, it } from 'vitest'
-import { Organization } from './organization'
+import { PasswordNotCorrectly } from './errors/password-not-correctly-error'
 
 describe('Organization', () => {
-  it('Should be able create a organization', () => {
-    const organization = new Organization({
-      name: 'organization example',
-      email: 'organization@email.com',
-      password: '123456',
-      address: 'street example',
-      cep: '99999000',
-      phoneNumber: '99999999999',
-    })
-
+  it('Should be able create an organization', () => {
+    const organization = MakeOrganization()
     expect(organization).toBeTruthy()
     expect(organization).toEqual(
       expect.objectContaining({
         name: organization.name,
       }),
     )
+  })
+
+  it('Should be not able create an organization with a password least one digit, one lower case, one upper case and special character', () => {
+    expect(() =>
+      MakeOrganization({
+        password: '123456',
+      }),
+    ).toThrow(PasswordNotCorrectly)
   })
 })
