f608172f8b6fe4e62b11718085fcb605164638c9
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-25 14:06:39 -0300

    tests(petsRepository): implemenht find many method

--- server/test/repositories/in-memory-pets-repository.ts
+++ server/test/repositories/in-memory-pets-repository.ts
@@ -1,5 +1,6 @@
 import { Pet } from '@/application/entities/pet'
 import { PetsRepository } from '@/application/repositories/pets-repository'
+import { SearchPetsParams } from '@/application/use-cases/search-pets'
 
 export class InMemoryPetsRepository implements PetsRepository {
   private pets: Pet[] = []
@@ -10,7 +11,22 @@ export class InMemoryPetsRepository implements PetsRepository {
     return pet
   }
 
-  async findMany(page: number) {
-    return this.pets.slice((page - 1) * 20, page * 20)
+  async findMany(
+    page: number,
+    city: string,
+    params: SearchPetsParams,
+  ): Promise<Pet[]> {
+    const hasParams = Object.entries(params).length > 0
+
+    if (hasParams) {
+      return this.pets
+        .filter((pet) => pet.city.toLowerCase() === city.toLowerCase())
+        .filter((pet) => pet.containsProps(params))
+        .slice((page - 1) * 20, page * 20)
+    }
+
+    return this.pets
+      .filter((pet) => pet.city === city)
+      .slice((page - 1) * 20, page * 20)
   }
 }
