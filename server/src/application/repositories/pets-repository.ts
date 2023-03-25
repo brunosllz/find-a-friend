714af4fd89a712853345c11993dc77787e985df6
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-25 14:04:24 -0300

    feat(petRepository): implement find many method

--- server/src/application/repositories/pets-repository.ts
+++ server/src/application/repositories/pets-repository.ts
@@ -1,6 +1,13 @@
 import { Pet } from '../entities/pet'
+import { SearchPetsParams } from '../use-cases/search-pets'
+
+export type QueryParams = SearchPetsParams
 
 export interface PetsRepository {
   create(pet: Pet): Promise<Pet>
-  findMany(page: number): Promise<Pet[]>
+  findMany(
+    page: number,
+    city: string,
+    queryParams?: QueryParams,
+  ): Promise<Pet[]>
 }
