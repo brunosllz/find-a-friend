c013ebee9383ecc10659debea2f492abe0ad3441
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-25 14:03:17 -0300

    tests(pet): adjusted validation on name of pet than created

--- server/src/application/entities/pet.spec.ts
+++ server/src/application/entities/pet.spec.ts
@@ -1,24 +1,16 @@
+import { MakePet } from 'test/factories/pet-factory'
 import { describe, expect, it } from 'vitest'
-import { Pet } from './pet'
 
 describe('Pet', () => {
   it('Should be able create a pet', () => {
-    const pet = new Pet({
-      name: 'princes',
-      age: 'elderly',
-      city: 'any where',
-      description: 'Is a pet',
-      energy: 5,
-      independence: 'high',
-      photo: 'https://www.pet-images.com/image.png',
-      size: 'big',
-      type: 'dog',
+    const pet = MakePet({
+      name: 'princess',
     })
 
     expect(pet).toBeTruthy()
     expect(pet).toEqual(
       expect.objectContaining({
-        name: pet.name,
+        name: 'princess',
       }),
     )
   })
