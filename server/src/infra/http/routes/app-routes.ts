5aab3405d34720c7f4f1c793a373519f65f71b45
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-27 10:01:56 -0300

    feat(routes): added fetch cities by state

--- server/src/infra/http/routes/app-routes.ts
+++ server/src/infra/http/routes/app-routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { FetchCitiesByStateController } from '../controllers/fetch-cities-by-state'
 import { FetchStatesController } from '../controllers/fetch-states'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/location/states', FetchStatesController)
+  app.get('/location/states', FetchStatesController)
+  app.get('/location/:acronymState/cities', FetchCitiesByStateController)
 }
