4c69df2f3e3891bb4117226c2a2e609b7ca29dc8
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-28 14:56:44 -0300

    feat(prisma): added pet repository

---
+++ server/src/infra/database/prisma/repositories/prisma-pets-repository.ts
@@ -0,0 +1,48 @@
+import { PetDTO } from '@/application/repositories/dto/pet-dto'
+import { PetsRepository } from '@/application/repositories/pets-repository'
+import { SearchPetsParams } from '@/application/use-cases/search-pets'
+import { prisma } from '@/infra/database/prisma'
+import { PrismaPetMapper } from '../mappers/prisma-pet-mapper'
+
+export class PrismaPetsRepository implements PetsRepository {
+  async create(pet: PetDTO) {
+    const raw = PrismaPetMapper.toPrisma(pet)
+
+    const rawPet = await prisma.pet.create({
+      data: raw,
+    })
+
+    return PrismaPetMapper.toDomain(rawPet)
+  }
+
+  async findById(id: string) {
+    const rawPet = await prisma.pet.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!rawPet) {
+      return null
+    }
+
+    return PrismaPetMapper.toDomain(rawPet)
+  }
+
+  async findMany(
+    page: number,
+    city: string,
+    queryParams?: SearchPetsParams | undefined,
+  ) {
+    const rawPets = await prisma.pet.findMany({
+      where: {
+        city,
+        ...queryParams,
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    })
+
+    return rawPets.map(PrismaPetMapper.toDomain)
+  }
+}
