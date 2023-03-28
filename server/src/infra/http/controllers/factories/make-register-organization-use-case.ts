43aacc090c0199a1c16911930b4bbcfe70e91012
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-03-28 16:16:57 -0300

    refactor(makeRegisterOrganizationUseCase): ajusted variables names

--- server/src/infra/http/controllers/factories/make-register-organization-use-case.ts
+++ server/src/infra/http/controllers/factories/make-register-organization-use-case.ts
@@ -1,9 +1,9 @@
 import { PrismaOrganizationsRepository } from '@/infra/database/prisma/repositories/prisma-organizations-repository'
-import { RegisterOrganizationUseCase } from '../../../../application/use-cases/register-organization'
+import { RegisterOrganizationUseCase } from '@/application/use-cases/register-organization'
 
 export function MakeRegisterOrganizationUseCase() {
-  const organizationRepository = new PrismaOrganizationsRepository()
-  const useCase = new RegisterOrganizationUseCase(organizationRepository)
+  const organizationsRepository = new PrismaOrganizationsRepository()
+  const useCase = new RegisterOrganizationUseCase(organizationsRepository)
 
   return useCase
 }
