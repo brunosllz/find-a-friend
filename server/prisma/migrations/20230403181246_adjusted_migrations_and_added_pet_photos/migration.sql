79429fc80ddb30feda9117856a1dd283c6b687b6
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-03 18:29:06 -0300

    feat(prisma): added pet photos and adjusted migrations

--- server/prisma/migrations/20230403181246_adjusted_migrations_and_added_pet_photos/migration.sql
+++ server/prisma/migrations/20230403181246_adjusted_migrations_and_added_pet_photos/migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE `organizations` (
     `address` VARCHAR(191) NOT NULL,
     `cep` VARCHAR(191) NOT NULL,
     `phoneNumber` VARCHAR(191) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `organizations_email_key`(`email`),
     PRIMARY KEY (`id`)
@@ -26,10 +26,23 @@ CREATE TABLE `pets` (
     `type` VARCHAR(191) NOT NULL,
     `photo` VARCHAR(191) NOT NULL,
     `org_id` VARCHAR(191) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `pet-images` (
+    `id` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+    `pet_id` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
 ALTER TABLE `pets` ADD CONSTRAINT `pets_org_id_fkey` FOREIGN KEY (`org_id`) REFERENCES `organizations`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `pet-images` ADD CONSTRAINT `pet-images_pet_id_fkey` FOREIGN KEY (`pet_id`) REFERENCES `pets`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ server/prisma/migrations/20230403185352_remove_photo_colunm_on_pet/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `photo` on the `pets` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `pets` DROP COLUMN `photo`;
