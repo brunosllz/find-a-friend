d563b0a286c3f7640165e0aebf703990f1beaca8
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-05 15:25:14 -0300

    feat(utils): added options to filter the pets

---
+++ web/src/utils/options-filter-pet.ts
@@ -0,0 +1,69 @@
+const age = [
+  {
+    value: 'cub',
+    placeholder: 'Filhote',
+  },
+  {
+    value: 'adolescent',
+    placeholder: 'Adolecente',
+  },
+  {
+    value: 'elderly',
+    placeholder: 'Velho',
+  },
+]
+
+const size = [
+  {
+    value: 'small',
+    placeholder: 'Pequeno',
+  },
+  {
+    value: 'medium',
+    placeholder: 'Médio',
+  },
+  {
+    value: 'big',
+    placeholder: 'Grande',
+  },
+]
+
+const independence = [
+  {
+    value: 'low',
+    placeholder: 'Baixa',
+  },
+  {
+    value: 'medium',
+    placeholder: 'Média',
+  },
+  {
+    value: 'high',
+    placeholder: 'Alta',
+  },
+]
+
+const energy = [
+  {
+    value: 1,
+    placeholder: '01',
+  },
+  {
+    value: 2,
+    placeholder: '02',
+  },
+  {
+    value: 3,
+    placeholder: '03',
+  },
+  {
+    value: 4,
+    placeholder: '04',
+  },
+  {
+    value: 5,
+    placeholder: '05',
+  },
+]
+
+export { age, size, independence, energy }
