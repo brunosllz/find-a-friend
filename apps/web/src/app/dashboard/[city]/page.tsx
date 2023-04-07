18207d512db5e1f33725ef904ec94b2fcbe775d1
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-06 21:57:32 -0300

    refactor(dashboard): implemented pet gallery and suspense loading

--- apps/web/src/app/dashboard/[city]/page.tsx
+++ apps/web/src/app/dashboard/[city]/page.tsx
@@ -1,6 +1,9 @@
+import { Suspense } from 'react'
 import { parseRouteParams } from '@/utils/parse-route-params'
 
 import { Sidebar } from '../(components)/Sidebar'
+import { Header } from '../(components)/Header'
+import { PetGallery } from '../(components)/PetGallery'
 
 interface DashboardProps {
   params: {
@@ -11,16 +14,18 @@ interface DashboardProps {
 export default async function Dashboard({ params }: DashboardProps) {
   const { city } = parseRouteParams(params)
 
-  const response = await fetch(`http://localhost:3333/pets/${city}`)
-  await response.json().then((data) => console.log(data))
-
   return (
     <div className="flex">
       <Sidebar />
-      <div className="bg-white flex flex-[1_1_auto] overflow-hidden pl-[392px]">
+      <div className="bg-[#FDECED] h-screen flex flex-[1_1_auto] overflow-hidden pl-[392px]">
         <div className="flex flex-[1_1_auto] overflow-hidden">
-          <div className="flex flex-[1_1_auto] h-full overflow-auto">
-            <p>PETS</p>
+          <div className="flex flex-[1_1_auto] flex-col h-full overflow-auto pl-8 pr-[112px]">
+            <Header />
+
+            <Suspense fallback={<div>Loading...</div>}>
+              {/* @ts-expect-error Async Component */}
+              <PetGallery city={city} />
+            </Suspense>
           </div>
         </div>
       </div>
