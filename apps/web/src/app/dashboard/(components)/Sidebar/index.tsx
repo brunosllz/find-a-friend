45f6d90bb22823d0d16be1e7ef238404c05f54d6
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-06 21:54:08 -0300

    styles(sidebar): added background color

--- apps/web/src/app/dashboard/(components)/Sidebar/index.tsx
+++ apps/web/src/app/dashboard/(components)/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import { SearchPetForm } from './SearchPetForm'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col h-screen fixed w-[392px] gap-8">
+    <aside className="flex flex-col h-screen fixed w-[392px] gap-8 bg-[#F15156]">
       <Header />
       <div className="flex flex-col gap-6 pr-10 pl-14">
         <strong className="font-extrabold text-xl">Filtros</strong>
