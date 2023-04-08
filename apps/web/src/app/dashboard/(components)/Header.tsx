f9b0371db8c5808557e3f95195e8482fd383a8e4
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-07 22:48:26 -0300

    refactor(header): implemented dynamic amount of pets

--- apps/web/src/app/dashboard/(components)/Header.tsx
+++ apps/web/src/app/dashboard/(components)/Header.tsx
@@ -3,15 +3,19 @@
 import { SelectInput } from '@/components/SelectInput'
 import { useForm } from 'react-hook-form'
 
-export function Header() {
+interface HeaderProps {
+  amountPets: number
+}
+
+export function Header({ amountPets }: HeaderProps) {
   const { register, control } = useForm()
 
   return (
     <header className="flex items-end w-full pb-6 h-[230px]">
       <div className="flex w-full items-center justify-between">
         <p className="text-[#0D3B66] ">
-          Encontre <span className="font-extrabold">324 amigos</span> na sua
-          cidade
+          Encontre <span className="font-extrabold">{amountPets} amigos</span>{' '}
+          na sua cidade
         </p>
 
         <div className="w-[210px]">
