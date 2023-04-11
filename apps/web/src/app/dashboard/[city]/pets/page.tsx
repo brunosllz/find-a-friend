d98996d4d946fb3f46d4543b6c3a5b648c29b7e0
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-10 23:13:52 -0300

    refactor: change route name of search pets by city

--- apps/web/src/app/dashboard/[city]/pets/page.tsx
+++ apps/web/src/app/dashboard/[city]/pets/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from 'react'
 import { parseRouteParams } from '@/utils/parse-route-params'
 
-import { Sidebar } from '../(components)/Sidebar'
-import { Header } from '../(components)/Header'
-import { PetGallery } from '../(components)/PetGallery'
+import { Sidebar } from '../../(components)/Sidebar'
+import { Header } from '../../(components)/Header'
+import { Pet, PetGallery } from '../../(components)/PetGallery'
 
 interface DashboardProps {
   params: {
@@ -14,13 +14,18 @@ interface DashboardProps {
 export default async function Dashboard({ params }: DashboardProps) {
   const { city } = parseRouteParams(params)
 
+  const response = await fetch(`http://localhost:3333/pets/${city}`)
+  const { count }: { pets: Pet[]; count: number } = await response
+    .json()
+    .then((data) => data)
+
   return (
     <div className="flex">
       <Sidebar />
       <div className="bg-[#FDECED] h-screen flex flex-[1_1_auto] overflow-hidden pl-[392px]">
         <div className="flex flex-[1_1_auto] overflow-hidden">
           <div className="flex flex-[1_1_auto] flex-col h-full overflow-auto pl-8 pr-[112px]">
-            <Header />
+            <Header amountPets={count} />
 
             <Suspense fallback={<div>Loading...</div>}>
               {/* @ts-expect-error Async Component */}
