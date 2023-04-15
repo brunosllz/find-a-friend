9c979daf55dc9c16f8a953cc106f88c05c2b7922
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-14 23:29:12 -0300

    feat(routes.pets): added get pet route

--- apps/server/src/infra/http/routes/pets-routes.ts
+++ apps/server/src/infra/http/routes/pets-routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { SearchPetsController } from '../controllers/pets/search'
+import { GetPetsController } from '../controllers/pets/get'
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.get('/pets/:city', SearchPetsController)
+  app.get('/pets/:id', GetPetsController)
+  app.get('/pets/location/:city', SearchPetsController)
 }
