b2f98ff9c297c1890a54e4059ae9b434481fb0f7
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-15 20:51:38 -0300

    feat(repository): added find many by id method on pet photos

--- apps/server/src/application/repositories/pets-photos-repository.ts
+++ apps/server/src/application/repositories/pets-photos-repository.ts
@@ -2,4 +2,5 @@ import { PetPhotoDTO } from './dto/pet-photo-dto'
 
 export interface PetsPhotosRepository {
   save(petPhotos: PetPhotoDTO[]): Promise<void>
+  findManyById(id: string): Promise<PetPhotoDTO[]>
 }
