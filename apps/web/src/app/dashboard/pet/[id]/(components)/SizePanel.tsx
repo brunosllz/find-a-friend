ba2db98b9b53e2a99cf9b92b14c18d9eef884b2a
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-16 14:39:17 -0300

    refactor(sizePainel): adjusted structure of component, now the props is dynamic

--- apps/web/src/app/dashboard/pet/[id]/(components)/SizePanel.tsx
+++ apps/web/src/app/dashboard/pet/[id]/(components)/SizePanel.tsx
@@ -1,8 +1,37 @@
 'use client'
 
+import { useState } from 'react'
 import clsx from 'clsx'
 
-export function SizePanel() {
+interface SizePanelProps {
+  size: 'small' | 'medium' | 'big'
+}
+
+export function SizePanel({ size }: SizePanelProps) {
+  const [currentSize] = useState<{
+    size: number
+    description: string
+  }>(() => {
+    if (size === 'small') {
+      return {
+        size: 0,
+        description: 'Pequenino',
+      }
+    }
+
+    if (size === 'medium') {
+      return {
+        size: 1,
+        description: 'Mediano',
+      }
+    }
+
+    return {
+      size: 2,
+      description: 'Grande',
+    }
+  })
+
   return (
     <div className="flex flex-col w-full h-24 items-start justify-between p-4 ring-2 ring-[#0D3B66] rounded-2xl text-[#0D3B66] text-lg font-semibold ring-opacity-10">
       <div className="flex gap-1 pt-[6px]">
@@ -10,12 +39,14 @@ export function SizePanel() {
           return (
             <div
               key={i}
-              className={clsx('h-3 w-3 rounded-full bg-[#0D3B66]', {})}
+              className={clsx('h-3 w-3 rounded-full bg-[#0D3B66]', {
+                'bg-[#CFD8E0]': currentSize.size < i,
+              })}
             />
           )
         })}
       </div>
-      Pequenino
+      {currentSize.description}
     </div>
   )
 }
