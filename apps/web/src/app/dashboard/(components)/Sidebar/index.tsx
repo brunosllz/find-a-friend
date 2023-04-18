8afa973a308c90b56e5ad34aa3cc11feb8652d76
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-18 12:37:42 -0300

    refactor(sidebar): added call to get selected state previous

--- apps/web/src/app/dashboard/(components)/Sidebar/index.tsx
+++ apps/web/src/app/dashboard/(components)/Sidebar/index.tsx
@@ -1,10 +1,31 @@
 import { Header } from './Header'
 import { SearchPetForm } from './SearchPetForm'
 
-export function Sidebar() {
+interface SidebarProps {
+  citySelected: string
+}
+
+export async function Sidebar({ citySelected }: SidebarProps) {
+  const statesResponse = await fetch('http://localhost:3333/location/states', {
+    method: 'GET',
+    next: { revalidate: 60 * 60 * 24 },
+  })
+
+  const statesData = await statesResponse
+    .json()
+    .then((data: Array<{ id: string; name: string; acronym: string }>) =>
+      data.map((item) => {
+        return {
+          id: item.id,
+          value: item.acronym,
+          placeholder: item.acronym,
+        }
+      }),
+    )
+
   return (
     <aside className="flex flex-col h-screen fixed w-[392px] gap-8 bg-[#F15156]">
-      <Header />
+      <Header states={statesData} citySelected={citySelected} />
       <div className="flex flex-col gap-6 pr-10 pl-14">
         <strong className="font-extrabold text-xl">Filtros</strong>
         <SearchPetForm />
