0ba1f0f1a05301fd77cefa5c4fc8ee71e801f76a
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-18 20:56:19 -0300

    refactor(phoneBanner): added dynamic phone number to card

--- apps/web/src/app/dashboard/pet/[id]/(components)/PhoneBanner.tsx
+++ apps/web/src/app/dashboard/pet/[id]/(components)/PhoneBanner.tsx
@@ -2,12 +2,16 @@
 
 import { WhatsappLogo } from 'phosphor-react'
 
-export function PhoneBanner() {
+interface PhoneBanerProps {
+  number: string
+}
+
+export function PhoneBanner({ number }: PhoneBanerProps) {
   return (
     <div className="flex bg-[#F3F5F7] py-3 px-6 gap-2 rounded-lg ml-20">
       <WhatsappLogo size={24} weight="fill" />
 
-      <span className="text-lg">81 1234.4567</span>
+      <span className="text-lg">{number}</span>
     </div>
   )
 }
