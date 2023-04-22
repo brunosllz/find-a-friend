5ed654d8f3bbd749671412c6f6af63eab7bc9cbd
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-22 20:07:54 -0300

    refactor(entity): changeded structure of pet entity, implement value object and apply extend class entity

--- apps/server/src/domain/entities/pet.ts
+++ apps/server/src/domain/entities/pet.ts
@@ -1,8 +1,26 @@
 import { randomUUID } from 'node:crypto'
 import { Replace } from '../helpers/Replace'
-import { PetPhoto } from './pet-photo'
+import { PetPhoto } from './value-objects/pet-photo'
+import { Entity } from '@/core/entities/entity'
+import { Description } from './value-objects/description'
+import { Energy } from './value-objects/energy'
 
 export type PetProps = {
+  name: string
+  description: Description
+  city: string
+  age: 'cub' | 'adolescent' | 'elderly'
+  energy: Energy
+  size: 'small' | 'medium' | 'big'
+  independence: 'low' | 'medium' | 'high'
+  type: 'dog' | 'cat'
+  photos: PetPhoto | null
+  orgId: string
+  createdAt: Date
+  [key: string]: any
+}
+
+export type CreatePetInput = {
   name: string
   description: string
   city: string
@@ -11,72 +29,26 @@ export type PetProps = {
   size: 'small' | 'medium' | 'big'
   independence: 'low' | 'medium' | 'high'
   type: 'dog' | 'cat'
-  photos: PetPhoto[] | null
-  orgId: string
-  createdAt: Date
+  photos: Array<{ url: string }> | null
+  orgId?: string
+  createdAt?: Date
 }
 
-export class Pet {
-  private props: Replace<PetProps, { [key: string]: any }>
-  private _id: string
-
-  private validateAmountEnergy(energy: number): boolean {
-    return energy > 0 && energy <= 5
-  }
-
-  private validateDescriptionLength(description: string): boolean {
-    return description.length > 0 && description.length <= 300
-  }
-
-  private validateAmountPetPhotos(
-    photos: Omit<PetPhoto, 'petId' | 'id'>[] | undefined,
-  ): boolean {
-    if (!photos) {
-      return true
-    }
-
-    return photos.length <= 5
-  }
-
-  constructor(
-    props: Replace<
-      PetProps,
+export class Pet extends Entity<PetProps> {
+  static create(props: CreatePetInput, id?: string) {
+    const pet = new Pet(
       {
-        createdAt?: Date
-        orgId?: string
-        photos?: Omit<PetPhoto, 'petId' | 'id'>[] | null
-      }
-    >,
-    id?: string,
-  ) {
-    this._id = id ?? randomUUID()
-
-    const energyAmountIsValid = this.validateAmountEnergy(props.energy)
-    if (!energyAmountIsValid) {
-      throw new Error('Energy amount is not valid.')
-    }
-
-    const descriptionLengthIsValid = this.validateDescriptionLength(
-      props.description,
+        ...props,
+        orgId: props.orgId ?? randomUUID(),
+        createdAt: props.createdAt ?? new Date(),
+        description: new Description(props.description),
+        energy: new Energy(props.energy),
+        photos: props.photos ? new PetPhoto(props.photos) : null,
+      },
+      id,
     )
-    if (!descriptionLengthIsValid) {
-      throw new Error('Description length is not valid.')
-    }
 
-    if (props.photos) {
-      const AmountPhotosIsValid = this.validateAmountPetPhotos(props.photos)
-
-      if (!AmountPhotosIsValid) {
-        throw new Error('Amount photos is not valid.')
-      }
-    }
-
-    this.props = {
-      ...props,
-      orgId: props.orgId ?? randomUUID(),
-      photos: props.photos ?? null,
-      createdAt: props.createdAt ?? new Date(),
-    }
+    return pet
   }
 
   public containsProps(
@@ -87,87 +59,43 @@ export class Pet {
     )
   }
 
-  public get id() {
-    return this._id
-  }
-
   public get name() {
     return this.props.name
-  }
-
-  public set name(name: string) {
-    this.props.name = name
   }
 
   public get description() {
     return this.props.description
   }
 
-  public set description(description: string) {
-    this.props.description = description
-  }
-
   public get city() {
     return this.props.city
-  }
-
-  public set city(city: string) {
-    this.props.city = city
   }
 
   public get age() {
     return this.props.age
   }
 
-  public set age(age: 'cub' | 'adolescent' | 'elderly') {
-    this.props.age = age
-  }
-
   public get energy() {
     return this.props.energy
-  }
-
-  public set energy(energy: number) {
-    this.props.energy = energy
   }
 
   public get size() {
     return this.props.size
   }
 
-  public set size(size: 'small' | 'medium' | 'big') {
-    this.props.size = size
-  }
-
   public get independence() {
     return this.props.independence
-  }
-
-  public set independence(independence: 'medium' | 'low' | 'high') {
-    this.props.independence = independence
   }
 
   public get type() {
     return this.props.type
   }
 
-  public set type(type: 'dog' | 'cat') {
-    this.props.type = type
-  }
-
-  public get photos(): Omit<PetPhoto, 'petId'>[] | null {
+  public get photos(): PetPhoto | null {
     return this.props.photos
-  }
-
-  public set photos(photos: Omit<PetPhoto, 'petId'>[] | null) {
-    this.props.photos = photos
   }
 
   public get orgId() {
     return this.props.orgId
-  }
-
-  public set orgId(orgId: string) {
-    this.props.orgId = orgId
   }
 }
