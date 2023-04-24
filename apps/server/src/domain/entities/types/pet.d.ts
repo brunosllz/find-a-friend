2871aa5224e8f6eaa9b6af9d13f09dc1de23e7fe
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-24 20:56:48 -0300

    feat(entity): added pet types

---
+++ apps/server/src/domain/entities/types/pet.d.ts
@@ -0,0 +1,29 @@
+/**
+ * Create pet input values
+ */
+
+export type CreatePetInput = {
+  name: string
+  description: string
+  city: string
+  age: 'cub' | 'adolescent' | 'elderly'
+  energy: number
+  size: 'small' | 'medium' | 'big'
+  independence: 'low' | 'medium' | 'high'
+  type: 'dog' | 'cat'
+  photos: Array<{ url: string }> | null
+  orgId?: string
+  createdAt?: Date
+}
+
+/**
+ * Props params to search a pet
+ */
+
+export type SearchPetsParams = {
+  age?: 'cub' | 'adolescent' | 'elderly'
+  energy?: number
+  size?: 'small' | 'medium' | 'big'
+  independence?: 'low' | 'medium' | 'high'
+  type?: 'dog' | 'cat'
+}
