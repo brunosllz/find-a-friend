51ef6283f717d46a90907c4ddc5e5df989a7f35c
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-26 10:16:45 -0300

    refactor(repository): changed parameters than pass to implement the methods of pet photos repository

--- apps/server/src/domain/repositories/pets-photos-repository.ts
+++ apps/server/src/domain/repositories/pets-photos-repository.ts
@@ -1,6 +1,6 @@
 import { PetPhotoDTO } from './dto/pet-photo-dto'
 
 export interface PetsPhotosRepository {
-  save(petPhotos: PetPhotoDTO[]): Promise<void>
-  findManyById(id: string): Promise<PetPhotoDTO[]>
+  save(petPhotos: Omit<PetPhotoDTO, 'id'>[], petId: string): Promise<void>
+  findManyById(petId: string): Promise<PetPhotoDTO[]>
 }
