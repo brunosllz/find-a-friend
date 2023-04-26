d3f4c1be61407bf18cfc3e12fd1de7243303a872
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-04-26 10:45:19 -0300

    chore(config): added app url type to config file

--- apps/server/src/infra/config/env.ts
+++ apps/server/src/infra/config/env.ts
@@ -2,8 +2,11 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
   PORT: z.coerce.number().default(3333),
+  APP_URL: z.string().url(),
 })
 
 const _env = envSchema.safeParse(process.env)
